76fad4424fb961139db8b3ea6d164daabeed5c60
zq.Yang <xiyaozhe@qq.com>
2021-08-14 22:27:30 +0800

    note-sync syncing

--- src/butil/resource_pool_inl.h
+++ src/butil/resource_pool_inl.h
@@ -89,6 +89,10 @@ static const size_t RP_GROUP_NBLOCK_NBIT = 16;
 static const size_t RP_GROUP_NBLOCK = (1UL << RP_GROUP_NBLOCK_NBIT);
 static const size_t RP_INITIAL_FREE_LIST_SIZE = 1024;
 
+// 该类用于计算一个block能存下几个T
+// 如果T大于64*1024 则，存储一个，否则按照 N1计算。 
+// 同时，一个block最大存储256个元素
+// 在 Block类中使用
 template <typename T>
 class ResourcePoolBlockItemNum {
     // Block 的内存大小为 64 * 1024; 元素大于1024 则该block只保存一个。
